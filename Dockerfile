c61869e9f7a39b7bd55f9ea5696fb97db57a601c
Haluk Ornek <hornek@redhat.com>
2025-08-26 10:45:51 +0200

    Added HTTP server feature

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
-# Dockerfile
 FROM registry.access.redhat.com/ubi8/openjdk-17
 
-COPY target/hello-world-java-1.0.0.jar /deployments/app.jar
+COPY target/hello-server-1.0.0.jar /deployments/app.jar
 
 ENV JAVA_APP_JAR=app.jar
+EXPOSE 8080
 
 CMD ["/opt/jboss/container/java/run/run-java.sh"]
 
--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,31 @@
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws IOException {
+        int port = 8080; // default OpenShift port
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+        System.out.println("Server started on port " + port);
+
+        server.createContext("/", new HelloHandler());
+        server.setExecutor(null); // default executor
+        server.start();
+    }
+
+    static class HelloHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            String response = "Hello, World!";
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+            try (OutputStream os = exchange.getResponseBody()) {
+                os.write(response.getBytes());
+            }
+        }
     }
 }
 
